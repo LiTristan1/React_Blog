263225fe3f2dba04be5e9c6b10667fb99e68546a
LiTristan <tristansup5@gmail.com>
2024-08-13 15:13:50 -0400

    Fixed prop loading error

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Feed from './Feed';
 import {useStoreState} from 'easy-peasy';
-const Home = (isLoading,fetchError) => {
+const Home = ({isLoading,fetchError}) => {
   const searchResults = useStoreState((state) => state.searchResults);
   return (
     <div className = 'Home'>
@@ -10,7 +10,7 @@ const Home = (isLoading,fetchError) => {
       {isLoading && <p>Data Loading</p>}
       {fetchError && <p>Error loading data {fetchError}</p>}
         {/**Pass data downt to feed */}
-        {!isLoading && !fetchError && searchResults.length ? (<Feed posts = {searchResults}/>):(<p style = {{marginTop: "2rem"}}> No Posts</p>)}
+        {<Feed posts = {searchResults}/>}
     </div>
   )
 }
